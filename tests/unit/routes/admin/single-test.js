310137f1bcb3f4fcd7b76da68befdf22415bf184
N ARAVIND RAKESH <78293623+NARAVINDR321@users.noreply.github.com>
2023-07-15 14:50:08 +0530

    Add files via upload

---
+++ tests/unit/routes/admin/single-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:admin/single', 'Unit | Route | admin/single', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
---
+++ tests/unit/routes/group-conversion-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:group-conversion', 'Unit | Route | group conversion', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
